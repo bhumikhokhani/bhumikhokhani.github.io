749221ee4178b0d25e03e9820122e570a72bca91
Bhumi Khokhani <bhumikhokhani@gmail.com>
2026-03-02 18:59:22 +0530

    Update loadWork.js

--- js/loadWork.js
+++ js/loadWork.js
@@ -1,17 +1,16 @@
 fetch('data/caseStudies.json')
-  .then(response => response.json())
+  .then(res => res.json())
   .then(data => {
     const grid = document.getElementById('work-grid');
 
     data.forEach(item => {
       const card = document.createElement('a');
-      card.href = item.link;
+      card.href = item.file;
       card.className = 'work-card';
 
       card.innerHTML = `
         <h4>${item.title}</h4>
         <p>${item.summary}</p>
-        <small>${item.impact}</small>
       `;
 
       grid.appendChild(card);
